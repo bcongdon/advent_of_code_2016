25a88176a36521b91ebfcbb3253276aaa7ad9b90
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-01 15:24:15 -0600

    Haskell parser for Day 1 txt

---
+++ 1.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ViewPatterns #-}
+
+
+import Data.List.Split
+import qualified Data.Text as Text
+
+
+data Direction = R | L deriving Show
+
+main = do
+    input <- readFile "1.txt"
+    let steps = [case head str of
+                                'L' -> (L, read (tail str) :: Int)
+                                'R' -> (R, read (tail str) :: Int)
+                | str <- splitOn ", " input]
+    print $ steps
