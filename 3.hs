7557552a7c858cdf1a71e0fea0461035b6f1369b
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-03 08:23:56 -0600

    Day 3 pt 1 in Haskell

---
+++ 3.hs
@@ -0,0 +1,17 @@
+valid_tri :: (Integral a) => [a] -> Bool
+valid_tri x = m < (s - m)
+    where m = maximum x
+          s = sum x
+
+count :: [Bool] -> Int
+count = length . filter (True==)
+
+line_to_int :: [Char] -> [Int]
+line_to_int l = [read i | i <- (words l)]
+
+main = do 
+    input <- readFile "3.txt"
+    let tris = map line_to_int $ lines input
+    let num_valid = count $ map valid_tri tris
+    putStrLn $ show num_valid
+    return 0
