fee58b772abe2e0b435bdff7887a17e286dc5fc8
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-07 06:39:08 -0600

    Day 7 pt 2

--- 7.py
+++ 7.py
@@ -1,4 +1,6 @@
 import re
+import itertools
+
 
 def contains_abba(a):
     if len(a) < 4:
@@ -6,6 +8,11 @@ def contains_abba(a):
     return any(a[i] == a[i+3] and a[i+1] == a[i+2] and
                a[i] != a[i+1]
                for i in range(len(a) - 3))
+
+
+def get_aba(a):
+    return [a[i:i+3] for i in range(len(a) - 2) if
+            a[i] == a[i+2] and a[i] != a[i+1]]
 
 
 def supports_tls(line):
@@ -21,13 +28,28 @@ def supports_tls(line):
     return found
 
 
+def supports_ssl(line):
+    pat = re.compile(r'\[|\]')
+    arr = pat.split(line)
+    abas = list(itertools.chain.from_iterable(map(get_aba, arr[0::2])))
+    babs = map(lambda i: '{0}{1}{0}'.format(i[1], i[0]), abas)
+    return any(any(x in i for x in babs) for i in arr[1::2])
+
+
 if __name__ == '__main__':
     assert supports_tls('abba[mnop]qrst')
     assert not supports_tls('abcd[bddb]xyyx')
     assert not supports_tls('aaaa[qwer]tyui')
     assert supports_tls('ioxxoj[asdfgh]zxcvbn')
 
+    assert supports_ssl('aba[bab]xyz')
+    assert not supports_ssl('xyx[xyx]xyx')
+    assert supports_ssl('aaa[kek]eke')
+    assert supports_ssl('zazbz[bzb]cdb')
+
     with open('7.txt', 'r') as f:
         lines = f.read().split()
-        s = sum(1 for i in lines if supports_tls(i))
-        print('Part 1: %s' % s)
+        s1 = sum(1 for i in lines if supports_tls(i))
+        s2 = sum(1 for i in lines if supports_ssl(i))
+        print('Part 1: %s' % s1)
+        print('Part 2: %s' % s2)
