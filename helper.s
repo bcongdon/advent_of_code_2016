9406180315fedc919fb728ebfa133bd5ad75aa9e
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-07 11:19:50 -0600

    Day 2 pt 1 in MIPS

---
+++ helper.s
@@ -0,0 +1,35 @@
+.data
+PRINT_INT = 1
+PRINT_STRING = 4
+PRINT_CHAR = 11
+
+
+.text
+
+# print string #########################################################
+#
+# argument $a0: string to print
+# returns       nothing
+
+.globl print_string
+print_string:
+        li      $v0, PRINT_STRING       # print string command
+        syscall                         # string is in $a0
+        jr      $ra
+
+
+# print int and space ##################################################
+#
+# argument $a0: number to print
+# returns       nothing
+
+.globl print_int_and_space
+print_int_and_space:
+        li      $v0, PRINT_INT  # load the syscall option for printing ints
+        syscall                 # print the number
+
+        li      $a0, ' '        # print a black space
+        li      $v0, PRINT_CHAR # load the syscall option for printing chars
+        syscall                 # print the char
+        
+        jr      $ra             # return to the calling procedure
