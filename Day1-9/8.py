7442cdfe4124d40a1c594436b6a0f5f848b86d72
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-08 06:51:01 -0600

    Added animation for part 2

--- Day1-9/8.py
+++ Day1-9/8.py
@@ -1,14 +1,19 @@
+# -*- coding: utf-8 -*-
 import itertools
+import sys
+from time import sleep
 
 
 def print_display(state):
+    sys.stdout.write('\033c')
+    out = ''
     for y in range(len(state[0])):
-        row = ''.join('#' if state[i][y] else '.' for i in range(len(state)))
-        print(row)
-    print('\n')
+        out += ''.join('█' if state[i][y] else ' ' for i in range(len(state)))
+        out += '\n'
+    print(out)
 
 
-def process_cmd_list(cmds):
+def process_cmd_list(cmds, animate=False):
     # pixels[x][y]
     #   inner lists are columns
     pixels = [[False for i in range(6)] for j in range(50)]
@@ -30,6 +35,9 @@ def process_cmd_list(cmds):
                 row = row[-amt:] + row[:-amt]
                 for i in range(len(pixels)):
                     pixels[i][loc] = row[i]
+        if animate:
+            print_display(pixels)
+            sleep(0.05)
     return sum(1 for i in pixels for j in i if j), pixels
 
 
@@ -37,7 +45,6 @@ if __name__ == '__main__':
     with open('8-test.txt', 'r') as f:
         assert process_cmd_list(f.readlines())[0] == 6
     with open('8.txt', 'r') as f:
-        amt, res = process_cmd_list(f.readlines())
+        amt, res = process_cmd_list(f.readlines(), animate=True)
         print("Part 1: %s" % amt)
-        print("Part 2:")
-        print_display(res)
+        print("Part 2: See above. :)")
