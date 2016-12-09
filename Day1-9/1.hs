4fdd18baa91da92b9adfbf4a8762236b2766733a
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-09 07:57:14 -0600

    Day 1 pt 1 in Haskell

--- Day1-9/1.hs
+++ Day1-9/1.hs
@@ -1,8 +1,31 @@
 import Data.List.Split
+import Data.List
+
 import qualified Data.Text as Text
 
 
+
 data Direction = R | L deriving Show
+
+turn compass dir = mod (compass + case dir of R -> 1
+                                              L -> -1) 4
+
+move prev (dir, n) = case last prev of
+                    (compass, x, y) -> case turn compass dir of
+                        0 -> [(0, x, y + i) | i <- [0..n]]
+                        1 -> [(1, x + i, y) | i <- [0..n]]
+                        2 -> [(2, x, y - i) | i <- [0..n]]
+                        3 -> [(3, x - i, y) | i <- [0..n]]
+
+part1 steps = 
+    foldr (++) [] $ scanl move [(0, 0, 0)] steps
+
+
+
+firstDup (x:xs) =
+    case elem x xs of
+        True -> x
+        False -> firstDup xs
 
 main = do
     input <- readFile "1.txt"
@@ -10,4 +33,9 @@ main = do
                                 'L' -> (L, read (tail str) :: Int)
                                 'R' -> (R, read (tail str) :: Int)
                 | str <- splitOn ", " input]
-    print $ steps
+    let (_, x, y) = last $ part1 steps
+    print $ "Part 1: " ++ show (x + y)
+    -- let (dx, dy) = part2 steps
+    -- print $ part1 steps
+    -- print $  part1 steps
+    -- print $ "Part 2: " ++ show (dx + dy)
